8b34edef7ce7d7b206776d030244aa7d6e2a1b9d
Rg12rg34 <rahulguptamca1@gmail.com>
2021-10-28 08:18:09 +0530

    Adding user registration format for Password Validation

--- src/UserRegistrationFormatValidation.java
+++ src/UserRegistrationFormatValidation.java
@@ -85,4 +85,19 @@ public class UserRegistrationFormatValidation {
             System.out.println("Enter Email Id According To Pattern: ^[0-9]{2}[0-9]{10}\s$");
         }
     }
+    public void passwordValidation(){
+        System.out.print("Enter Password: ");
+        String password = Sc.next();
+        /*String regexPassword have:
+           Password must have minimum 8 characters
+         */
+        String regexPassword = "^[A-Z a-z]{8,}$";
+        pattern = Pattern.compile(regexPassword);
+        matcher = pattern.matcher(password);
+        Boolean passwordIs = matcher.matches();
+        System.out.println(passwordIs);
+        if (!passwordIs){
+            System.out.println("Enter Email Id According To Pattern: [A-Z a-z]{8,}");
+        }
+    }
 }
--- src/com/BridgeLabz/UserRegistrationFormatValidationMain.java
+++ src/com/BridgeLabz/UserRegistrationFormatValidationMain.java
@@ -12,6 +12,7 @@ public class UserRegistrationFormatValidationMain {
         System.out.println("Enter 2 : to Validate Last Name ");
         System.out.println("Enter 3 : to Validate Email ID ");
         System.out.println("Enter 4 : to Validate Phone Number ");
+        System.out.println("Enter 5 : to Validate Password ");
 
         int choice = 0;
         while (choice == 0) {
@@ -27,6 +28,9 @@ public class UserRegistrationFormatValidationMain {
                     break;
                 case 4:
                     Validate.phoneNumberValidation();
+                    break;
+                case 5:
+                    Validate.passwordValidation();
                     break;
                 default:
                     System.out.println("Enter Valid Choice");
