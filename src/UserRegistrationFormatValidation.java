658b28c36cf70f0dc6a22ba6c1865ee4edc9ce8b
Rg12rg34 <rahulguptamca1@gmail.com>
2021-10-28 08:01:41 +0530

    Adding user registration format for phone number validation

--- src/UserRegistrationFormatValidation.java
+++ src/UserRegistrationFormatValidation.java
@@ -28,6 +28,7 @@ public class UserRegistrationFormatValidation {
             System.out.println("Enter First Name According To Pattern: [A-Z]{1}+[a-z]{2,}");
         }
     }
+
     //Created A Method For Last Name Validation
     public void lastNameValidation(){
         System.out.print("Enter Last Name: " );
@@ -44,6 +45,44 @@ public class UserRegistrationFormatValidation {
         System.out.println(lastNameIs);
         if (!lastNameIs){
             System.out.println("Enter Last Name According To Pattern: [A-Z]{1}+[a-z]{2,}");
+        }
+    }
+
+    //Created A Method For Email ID Validation
+    public void emailIdValidation(){
+        System.out.print("Enter Email Id: " );
+        String emailId = Sc.next();
+
+        /* RegexEmailId Pattern have Total 5 parts: 3 Mandatory & 2 Optional
+            Example:- Abc.xyx@gmail.com.in
+            "Abc, @gmail, .com" Are mandatory Parts In This Pattern
+            ".xyz & .in" Are Optional Part In This Pattern
+        */
+        String regexEmailId = "^[A-Z a-z 1-9]+([.][A-Z a-z 1-9]+)*@[A-Z a-z]{3,5}+.[a-z]{2,3}+([.][a-z]{2})*$";
+        pattern = Pattern.compile(regexEmailId);
+        matcher = pattern.matcher(emailId);
+        Boolean emailIdIs = matcher.matches();
+        System.out.println(emailIdIs);
+        if (!emailIdIs){
+            System.out.println("Enter Email Id According To Pattern: ^[A-Z a-z 1-9]+([.][A-Z a-z 1-9]+)*@[A-Z a-z]{3,5}+.[a-z]{2,3}+([.][a-z]{2})*$");
+        }
+    }
+
+    //Created A Method For Phone Number Validation
+    public void phoneNumberValidation(){
+        System.out.print("Enter Phone Number: ");
+        String phoneNumber = Sc.next();
+        /*String regexPhoneNumber have 2 parts:
+            1st part is for Country Code
+            2nd part is for 10-digit number
+         */
+        String regexPhoneNumber = "^[0-9]{2}[0-9]{10}$";
+        pattern = Pattern.compile(regexPhoneNumber);
+        matcher = pattern.matcher(phoneNumber);
+        Boolean phoneNumberIs = matcher.matches();
+        System.out.println(phoneNumberIs);
+        if (!phoneNumberIs){
+            System.out.println("Enter Email Id According To Pattern: ^[0-9]{2}[0-9]{10}\s$");
         }
     }
 }
