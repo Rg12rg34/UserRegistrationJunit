5d3fb7c32a9e1ea2082a0edac52c08823eadbc2f
Rg12rg34 <rahulguptamca1@gmail.com>
2021-10-28 07:07:07 +0530

    Welcome to user registration

---
+++ src/com/BridgeLabz/UserRegistrationFormatValidationMain.java
@@ -0,0 +1,16 @@
+package com.BridgeLabz;
+import java.util.Scanner;
+public class UserRegistrationFormatValidationMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To User Registration Format Validation Using Regex Program");
+        UserRegistrationFormatValidation Validate = new UserRegistrationFormatValidation();
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter 1 : to validate First Name ");
+        switch (Sc.nextInt()){
+            case 1:
+                Validate.firstNameValidation();
+                break;
+        }
+    }
+}
